29f652c6604ebc37d2250ee00083d16d314528fb
Luca Stubbe <60596337+stubbelj@users.noreply.github.com>
2023-10-22 15:53:22 -0700

    simple walk cycle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    Animator anim;
     public float speed = 1.0f;
     public LayerMask interactLayer;
     public float interactDis = 1.0f;
+    public bool[] prevInputs = new bool[]{false, false, false, false}; //wasd
+    public Sprite[] standing;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,7 +30,7 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.D))
         {
-            sprite.flipY = false;
+            sprite.flipX = false;
             rb.velocity += Vector2.right * speed;
         }
         if (Input.GetKey(KeyCode.S))
@@ -35,8 +39,53 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.A))
         {
-            sprite.flipY = true;
+            sprite.flipX = true;
             rb.velocity += Vector2.left * speed;
+        }
+
+        //play walking anims
+        if (Input.GetKey(KeyCode.W)) {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)) {
+                anim.Play("player_walk_upRight");
+            } else {
+                anim.Play("player_walk_up");
+            }
+        } else if (Input.GetKey(KeyCode.S)) {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)) {
+                anim.Play("player_walk_downRight");
+            } else {
+                anim.Play("player_walk_down");
+            }
+        } else {
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)) {
+                anim.Play("player_walk_right");
+            }
+        }
+
+        // if no new inputs, player stops walking and sets standing sprite
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D)) {
+            anim.enabled = false;
+            if (prevInputs[0]) {
+                if (prevInputs[3] || prevInputs[1]) {
+                    sprite.sprite = standing[3];
+                } else {
+                    sprite.sprite = standing[4];
+                }
+            } else if (prevInputs[2]) {
+                if (prevInputs[3] || prevInputs[1]) {
+                    sprite.sprite = standing[1];
+                } else {
+                    sprite.sprite = standing[0];
+                }
+            } else if (prevInputs[3] || prevInputs[1]) {
+                    sprite.sprite = standing[2];
+            } else {
+                print("setting sprite");
+                sprite.sprite = standing[0];
+            }
+        } else {
+            prevInputs = new bool[4]{Input.GetKey(KeyCode.W), Input.GetKey(KeyCode.A), Input.GetKey(KeyCode.S), Input.GetKey(KeyCode.D)};
+            anim.enabled = true;
         }
         
         //Interact With Object
