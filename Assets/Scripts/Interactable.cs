9b9f473e25156c0c589cc1866f4c5890cfedaff7
MatthewMeacham305 <mightymatthewme@gmail.com>
2023-10-30 19:18:32 -0700

    Added some Sound functionality

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -9,18 +9,22 @@ using UnityEngine.UI;
 public class Interactable : MonoBehaviour
 {
     [SerializeField] private string words = "Test string\nSecond Line\nThird Line";
+    [SerializeField] private AudioClip interactSound;
+    public bool loopAudio = false;
+    public float volume = 1.0f;
     private string blackOutStart = "<mark=#000000ff>";
     private string blackOutEnd = "</mark>";
     private string blackOut;
     private TextMeshProUGUI frontText;
     private TextMeshProUGUI backText;
+    private AudioSource soundPlayer;
     [HideInInspector] public bool typing = false;
     // Start is called before the first frame update
     void Start()
     {
         frontText = GameObject.Find("FrontText").GetComponent<TextMeshProUGUI>();
         backText = GameObject.Find("BackText").GetComponent<TextMeshProUGUI>();
-        //TypeWords();
+        soundPlayer = gameObject.AddComponent<AudioSource>();
     }
 
     public void TypeWords()
@@ -31,6 +35,13 @@ public class Interactable : MonoBehaviour
         backText.text = blackOut;
         frontText.text = "";
         StartCoroutine(CharByChar(chars));
+        if (interactSound != null)
+        {
+            soundPlayer.clip = interactSound;
+            soundPlayer.loop = loopAudio;
+            soundPlayer.volume = volume;
+            soundPlayer.Play();
+        }
     }
 
     IEnumerator CharByChar(char[] chars)
@@ -43,9 +54,13 @@ public class Interactable : MonoBehaviour
             yield return new WaitForSeconds(0.1f);
         }
         typing = false;
+        soundPlayer.Stop();
         //cooldown before dissapearing
         yield return new WaitForSeconds(3);
-        backText.text = "";
-        frontText.text = "";
+        if (frontText.text == words)
+        {
+            backText.text = "";
+            frontText.text = "";
+        }
     }
 }
